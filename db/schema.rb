531b8cc4fffa933c195a41e3a9764a5ae1076d75
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-02 18:12:09 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,7 @@ ActiveRecord::Schema.define(version: 20180702175921) do
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.datetime "release_date"
+    t.string   "director"
     t.string   "lead"
     t.boolean  "in_theaters"
   end
