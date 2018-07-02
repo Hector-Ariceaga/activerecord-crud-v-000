a311f50c467bae636a24d09f576a55ec0ebb6e5f
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-02 18:51:33 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -24,7 +24,7 @@ end
 
 def can_be_created_in_a_block(args = {})
   Movie.create do |m|
-    args.each do |attribute, value|
+    m.each do |attribute, value|
       self.send("#{attribute}=", value)
     end 
   end
