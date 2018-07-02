7e40641d8e261e50cedad0dcd768f165a58d8033
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-02 18:23:35 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,9 +22,10 @@ def can_be_created_with_a_hash_of_attributes
   movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = )
+def can_be_created_in_a_block(args)
   Movie.create do |m|
-    __
+    m.each do |args|
+      
   end
 end
 
